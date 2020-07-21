b8504ebf57e5006d98adc09bc72ba2151f05bfba
Alexander Kraus <nr4@z10.info>
2020-07-21 10:22:32 +0200

    removed comments from scomp

--- cmake/scomp.cpp
+++ cmake/scomp.cpp
@@ -1,99 +1,20 @@
 
-// /* cozy - 4k executable gfx entry by NR4/Team210, shown at Under Construction 2k19
-//  * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
-//  *
-//  * This program is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU General Public License as published by
-//  * the Free Software Foundation, either version 3 of the License, or
-//  * (at your option) any later version.
-//  *
-//  * This program is distributed in the hope that it will be useful,
-//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  * GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License
-//  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//  */
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// int WINAPI demo(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-// {
-//     // char **cmdargs = GetCommandLine();
-//     // printf("%d %s\n", nCmdShow, pCmdLine[0]);
-//     // printf("Here.\n");
-//     // int argc;
-//     // LPWSTR *args = CommandLineToArgvW(pCmdLine, &argc);
-//     // printf("%s\n", args[0]);
-//     // Display demo window
-// CHAR WindowClass[] = "T";
-
-// WNDCLASSEX wc = {0};
-// wc.cbSize = sizeof(wc);
-// wc.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
-// wc.lpfnWndProc = &DefWindowProc;
-// wc.cbClsExtra = 0;
-// wc.cbWndExtra = 0;
-// wc.hInstance = hInstance;
-// wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
-// wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-// wc.hbrBackground = NULL;
-// wc.lpszMenuName = NULL;
-// wc.lpszClassName = WindowClass;
-// wc.hIconSm = NULL;
-
-// RegisterClassEx(&wc);
-
-// HWND hwnd = CreateWindowEx(0, WindowClass, "", WS_POPUP | WS_VISIBLE, 0, 0, 1920, 1080, NULL, NULL, hInstance, 0);
-
-// DEVMODE dm = {0};
-// dm.dmSize = sizeof(dm);
-// dm.dmPelsWidth = 1920;
-// dm.dmPelsHeight = 1080;
-// dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
-
-// ShowWindow(hwnd, FALSE);
-// UpdateWindow(hwnd);
-
-// PIXELFORMATDESCRIPTOR pfd =
-//     {
-//         sizeof(PIXELFORMATDESCRIPTOR),
-//         1,
-//         PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, //Flags
-// PFD_TYPE_RGBA,                                              // The kind of framebuffer. RGBA or palette.
-// 32,                                                         // Colordepth of the framebuffer.
-//         0, 0, 0, 0, 0, 0,
-//         0,
-//         0,
-//         0,
-//         0, 0, 0, 0,
-//         24, // Number of bits for the depthbuffer
-//         8,  // Number of bits for the stencilbuffer
-//         0,  // Number of Aux buffers in the framebuffer.
-//         PFD_MAIN_PLANE,
-//         0,
-//         0, 0, 0};
-
-// HDC hdc = GetDC(hwnd);
-
-// int pf = ChoosePixelFormat(hdc, &pfd);
-// SetPixelFormat(hdc, pf, &pfd);
-
-// HGLRC glrc = wglCreateContext(hdc);
-// wglMakeCurrent(hdc, glrc);
-
-// glCreateShader = (PFNGLCREATESHADERPROC)wglGetProcAddress("glCreateShader");
-// glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
-// glCompileShader = (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
-// glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog");
-// glGetShaderiv = (PFNGLGETSHADERIVPROC)wglGetProcAddress("glGetShaderiv");
-
-//     PostQuitMessage(0);
-
-//     return 0;
-// }
+/* scomp - compile a shader and see if it will work in the demo.
+ * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <windows.h>
 #include <stdio.h>
